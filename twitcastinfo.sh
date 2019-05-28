224dcda38bc76dd4f383c91df20cefce2c7005e5
lovezzzxxx <1067426979@qq.com>
2019-05-28 18:36:14 +0800

    Update twitcastinfo.sh

--- twitcastinfo.sh
+++ twitcastinfo.sh
@@ -4,9 +4,7 @@ PART_URL="${1}"
 PAGE=0
 PAGE_LINK="show/0"
 while true; do
-	echo ${PAGE_LINK}
-
-	curl -s "https://twitcasting.tv/${PART_URL}/${PAGE_LINK}" | awk 'BEGIN{RS="<a class=\"tw-movie-thumbnail\"|<div class=\"paging\"";FS="[\n<>]";ORS="\n";OFS="\t"} $1 ~ /href/ {if (NF<100) {split($1,LINK,"\"");TITLE=$18;gsub("^ *| *$","",TITLE);DATE=$30;print "NO_REC",LINK[2],DATE,TITLE} ; if (NF>100) {split($1,LINK,"\"");TITLE=$30;gsub("^ *| *$","",TITLE);DATE=$42;print "RECORD",LINK[2],DATE,TITLE}}'
+	curl -s "https://twitcasting.tv/${PART_URL}/${PAGE_LINK}" | awk 'BEGIN{RS="<a class=\"tw-movie-thumbnail\"|<div class=\"paging\"";FS="[\n<>]";ORS="\n";OFS="\t"} $1 ~ /href/ {split($1,LINK,"\"") ; TITLE_POS=match($0,"<span class=\"tw-movie-thumbnail-title\">[^<]*</span>");TITLE=substr($0,TITLE_POS,RLENGTH);gsub("^<span class=\"tw-movie-thumbnail-title\">\n *| *</span>$","",TITLE) ; DATE_POS=match($0,"[0-9]*/[0-9]*/[0-9]* [0-9]*:[0-9]*:[0-9]*");DATE=substr($0,DATE_POS,19) ; if (NF<100) print "NO_REC",LINK[2],DATE,TITLE ; if (NF>100) print "RECORD",LINK[2],DATE,TITLE}'
 	
 	PAGE=$(( ${PAGE} + 1 ))
 	PAGE_LINK=$(curl -s "https://twitcasting.tv/${PART_URL}/${PAGE_LINK}" | grep -o "show/${PAGE}-[0-9]*")
