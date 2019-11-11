9858a55525393a05e3ccb6ab4317b5d272c6c8f3
lovezzzxxx <1067426979@qq.com>
2019-11-12 00:23:10 +0800

    Update twitcast.sh

--- twitcast.sh
+++ twitcast.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 if [[ ! -n "${1}" ]]; then
-	echo "${0} 频道编号"
+	echo "${0} 频道编号 获取此时间之后的评论"
 	exit 1
 fi
 
 
 
 PART_URL="${1}"
+DATE_AFTER="${2}" ; [[ "${DATE_AFTER}" == "" ]] || DATE_AFTER_TIMESTAMP=$(date -d "${DATE_AFTER}" +%s)
+
+
 
 ./twitcastinfo.sh "${PART_URL}" > "./twitcastinfo_${PART_URL/:/：}.log" ; 
 mkdir "twitcastcomment_${PART_URL/:/：}"
-for LINK in $(awk -F"[\t(]" '{print $1}' "./twitcastinfo_${PART_URL/:/：}.log"); do 
-	NAME=$(echo ${LINK} | sed 's/\//_/g' | sed 's/:/：/g')
-	./twitcastcomment.sh "${LINK}" > "./twitcastcomment_${PART_URL/:/：}/twitcastcomment${NAME}.log"; 
+awk -F"\t" '{print $1}' "./twitcastinfo_${PART_URL/:/：}.log" | while read LINE; do
+	LINK=$(echo ${LINE} | awk -F"[()]" '{print $1}') ; NAME=$(echo ${LINK} | sed 's/\//_/g' | sed 's/:/：/g')
+	DATE=$(echo ${LINE} | awk -F"[()]" '{print $2}') ; DATE_TIMESTAMP=$(date -d "${DATE}" +%s)
+	if [[ "${DATE_AFTER}" == "" ]] || [[ "${DATE_TIMESTAMP}" -gt "${DATE_AFTER_TIMESTAMP}" ]]; then
+		./twitcastcomment.sh "${LINK}" > "./twitcastcomment_${PART_URL/:/：}/twitcastcomment${NAME}.log"
+	fi
 done
