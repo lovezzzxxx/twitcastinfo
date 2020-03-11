ff8540ac62998b210313bc8430d77c601f0a483d
lovezzzxxx <1067426979@qq.com>
2020-03-11 21:07:41 +0800

    Update twitcast.sh

--- twitcast.sh
+++ twitcast.sh
@@ -12,9 +12,9 @@ fi
 PART_URL=$(echo ${1} | sed 's/https:\/\/twitcasting.tv\///g')
 DATE_AFTER="${2}" ; [[ "${DATE_AFTER}" == "" ]] || DATE_AFTER_TIMESTAMP=$(date -d "${DATE_AFTER}" +%s)
 
-./twitcastinfo.sh "${PART_URL}" > "./twitcastinfo_${PART_URL/:/：}.log" ; 
+./twitcastuser.sh "${PART_URL}" > "./twitcastuser_${PART_URL/:/：}.log" ; 
 mkdir "twitcastcomment_${PART_URL/:/：}"
-awk -F"\t" '{print $1}' "./twitcastinfo_${PART_URL/:/：}.log" | while read LINE; do
+awk -F"\t" '{print $1}' "./twitcastuser_${PART_URL/:/：}.log" | while read LINE; do
 	LINK=$(echo ${LINE} | awk -F"[()]" '{print $1}') ; NAME=$(echo ${LINK} | sed 's/\//_/g' | sed 's/:/：/g')
 	DATE=$(echo ${LINE} | awk -F"[()]" '{print $2}') ; DATE_TIMESTAMP=$(date -d "${DATE}" +%s)
 	if [[ "${DATE_AFTER}" == "" ]] || [[ "${DATE_TIMESTAMP}" -gt "${DATE_AFTER_TIMESTAMP}" ]]; then
